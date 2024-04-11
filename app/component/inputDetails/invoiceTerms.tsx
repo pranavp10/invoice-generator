a5c85d0c8a6b782c4c78f5ce7f805f6e26551e18
Pranav <pranavkp.me@outlook.com>
2024-04-11 07:52:40 +0530

    fix: build

--- app/component/inputDetails/invoiceTerms.tsx
+++ app/component/inputDetails/invoiceTerms.tsx
@@ -1,6 +1,6 @@
 "use client";
 import CustomTextInput from "@/app/component/ui/customTextInput";
-import CustomDateInput from "@/app/component/ui/customDateInput";
+// import CustomDateInput from "@/app/component/ui/customDateInput";
 
 const InvoiceTerms = () => (
   <div className="pt-24">
@@ -10,8 +10,8 @@ const InvoiceTerms = () => (
       placeholder="INVOICE-01"
       variableName="invoiceNo"
     />
-    <CustomDateInput label="Issue date" variableName="issueDate" />
-    <CustomDateInput label="Due date" variableName="dueDate" />
+    {/* <CustomDateInput label="Issue date" variableName="issueDate" />
+    <CustomDateInput label="Due date" variableName="dueDate" /> */}
   </div>
 );
 
