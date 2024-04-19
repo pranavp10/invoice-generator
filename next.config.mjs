2f2eaf7f1082bf22d0a6f872cd9afe99d29980c7
Pranav <pranavkp.me@outlook.com>
2024-04-19 21:42:51 +0530

    remove glitch tip

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,4 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(
-  nextConfig,
-  {
-    silent: true,
-    org: "prolab",
-    project: "invoice-generator",
-    url: "https://glitchtip.prolab.sh/",
-  },
-  {
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true,
-  }
-);
+export default nextConfig;
