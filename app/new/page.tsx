cf8d53b6078c42b7bf2cc9fa2cdf70522a99a78b
Pranav <pranavkp.me@outlook.com>
2024-05-28 20:04:15 +0530

    added suspense for the new page component

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,8 +1,11 @@
 import { NewInvoiceForm } from "@/app/new/component/NewInvoiceForm";
+import { Suspense } from "react";
 
 const Page = () => (
   <div className="min-h-screen overflow-y-auto h-full flex items-center md:flex-row flex-col-reverse">
-    <NewInvoiceForm />
+    <Suspense>
+      <NewInvoiceForm />
+    </Suspense>
   </div>
 );
 
